641541defb2c448221a5414f17b250210fc862a5
Dominique Skaf <doskaf@yahoo.com>
2019-10-23 21:34:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,9 +24,6 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
-  # If no arguments are passed, use default values:
-  # title == "Home Alone"
-  # release_date == 1990
   
   Movie.create do |m|
     m.title = args[:title]
@@ -35,7 +32,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  sql = <<-SQL
+    SELECT * FROM movies ORDER BY id LIMIT 1
+  SQL
 end
 
 def can_get_the_last_item_in_the_database
